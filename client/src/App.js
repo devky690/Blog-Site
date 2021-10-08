9b50b9ed2246644b748da4435c6c6aeb30a68041
Kyle Abrahams <kyleabrahams@Kyles-MacBook-Air.local>
2021-10-08 14:12:55 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="section-heading">
+        <h1>Cloud of Ideas</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
